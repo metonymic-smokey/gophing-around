f637fc0c08f4fc702137138a531e10ce27b56f9b
metonymic-smokey <ahuja.aditi@gmail.com>
2020-07-04 11:30:40 +0530

    minor changes

--- HTMLParser/README.md
+++ HTMLParser/README.md
@@ -1,4 +1,6 @@
 # HTML Link Parser
 
-This is simple link parser that returns the text found in a link attribute in a HTML file.
+This is simple link parser that returns the text found in a link attribute in a HTML file.  
 
+Usage:
+`go run text_token.go [path to HTML file]`    
--- HTMLParser/text_token.go
+++ HTMLParser/text_token.go
@@ -15,56 +15,56 @@ type Link struct {
 }
 
 func text(n *html.Node) string {
-    if(n.Type == html.TextNode) {
-        return n.Data
-    }
-    if(n.Type != html.ElementNode) {
-        return ""
-    }
-    var ret string
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+	if n.Type != html.ElementNode {
+		return ""
+	}
+	var ret string
 
-    for c:=n.FirstChild;c!=nil;c=c.NextSibling {
-        ret+=text(c)
-    }
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret += text(c)
+	}
 
-    return ret
+	return ret
 
 }
 
 func parse(s string) []Link {
-    var links []Link
+	var links []Link
 
-    doc,err := html.Parse(strings.NewReader(s))
-    if(err!=nil) {
-        log.Fatal(err)
-    }
+	doc, err := html.Parse(strings.NewReader(s))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    var f func(*html.Node)
+	var f func(*html.Node)
 
-    f = func(n *html.Node)  {
+	f = func(n *html.Node) {
 		var l Link
 		if n.Type == html.ElementNode && n.Data == "a" {
 			for _, a := range n.Attr {
 				if a.Key == "href" {
 					l.Href = a.Val
-                    b:=text(n)
-                    b = strings.TrimSpace(b)
-                    l.Text = b
-                    links = append(links, l)
+					b := text(n)
+					b = strings.TrimSpace(b)
+					l.Text = b
+					links = append(links, l)
 					break
 
 				}
 			}
-            		    
-        }
+
+		}
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
-            f(c)
+			f(c)
 		}
 	}
 
-    f(doc)
+	f(doc)
 
-    return links
+	return links
 }
 
 func main() {
@@ -83,10 +83,10 @@ func main() {
 
 	links := parse(s)
 
-    fmt.Println("Format:")
-    fmt.Println("Link: Text")
+	fmt.Println("Format:")
+	fmt.Println("Link: Text")
 	for _, l := range links {
-        fmt.Println(l.Href,": ",l.Text)
+		fmt.Println(l.Href, ": ", l.Text)
 	}
 
 }
